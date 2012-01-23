3a011a627f666465de666e6251d4ed264c5cf824
Matt Elliott <melliott@ccistudios.com>
2012-01-23 14:57:50 -0500

    cleaned up and added js to keep the class of parent

--- template/templates/KHBNS/js/tabs.js
+++ template/templates/KHBNS/js/tabs.js
@@ -1,39 +1,41 @@
 window.addEvent('domready', function () {
-$$('.tab-container').each(function (container) {
-$nav = new Element('ul', {
-'class': 'tabs'
-});
-$panels = new Element('div');
-$modules = container.getElements('.moduletable').dispose();
+	$$('.tab-container').each(function (container) {
+		$nav = new Element('ul', {
+			'class': 'tabs'
+		});
+		$panels = new Element('div');
+		$modules = container.getElements('.moduletable').dispose();
 
-$modules.getElements('> h3').each(function (h3) {
-(new Element('li', {
-text: h3.get('html')
-})).inject($nav);
-});
-$nav.getElement('li').addClass('active');
+		$modules.getElements('> h3').each(function (h3) {
+			(new Element('li', {
+				text: h3.get('html'),
+				'class': h3.getParent('.moduletable, .module').getProperty('class')[0].replace('moduletable', '').replace('module', '')
+			})).inject($nav);
+			
+		});
+		$nav.getElement('li').addClass('active');
 
-$modules.getElements('> div').each(function (content) {
-(new Element('div', {
-html: content.get('html'),
-'class': 'panel'
-})).inject($panels);
-});
-$panels.getElement('div').addClass('active');
+		$modules.getElements('> div').each(function (content) {
+			(new Element('div', {
+				html: content.get('html'),
+				'class': 'panel ' + content.getParent('.moduletable, .module').getProperty('class')[0].replace('moduletable', '').replace('module', '')
+			})).inject($panels);
+		});
+		$panels.getElement('div').addClass('active');
 
-$nav.getElements('li').addEvent('click', function (event) {
-menu = $nav.getElements('li');
-menu.removeClass('active');
-event.target.addClass('active');
-index = menu.indexOf(event.target);
+		$nav.getElements('li').addEvent('click', function (event) {
+			menu = $nav.getElements('li');
+			menu.removeClass('active');
+			event.target.addClass('active');
+			index = menu.indexOf(event.target);
 
-contents = $panels.getElements('> div');
-contents.removeClass('active');
-contents[index].addClass('active');
-});
+			contents = $panels.getElements('> div');
+			contents.removeClass('active');
+			contents[index].addClass('active');
+		});
 
-$nav.inject(container);
-$panels.inject(container);
+		$nav.inject(container);
+		$panels.inject(container);
 
-});
+	});
 });
