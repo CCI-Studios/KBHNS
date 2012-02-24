fb1aa74758afcbd1febddddcef710a87e890019b
Matt Elliott <melliott@ccistudios.com>
2012-02-24 14:39:43 -0500

    script fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 set :branch, "master"
 
 # This may be the same as your `Web` server
-role :app, "ccistudios.com"
+role :app, "ccistaging.com"
 
 # directories
 set :deploy_to, "/home/staging/subdomains/kbhns"
