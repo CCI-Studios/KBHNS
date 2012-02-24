64a5906897dc66455e92b311f97ca2640eeacece
Matt Elliott <melliott@ccistudios.com>
2012-02-24 14:53:16 -0500

    script fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,6 @@ set :branch, "master"
 role :app, "kbhns.ccistaging.com"
 
 # directories
-set :deploy_to, "/home/staging/subdomains/kbhns"
+set :deploy_to, "/home/staging/subdomains/KBHNS"
 set :public, "#{deploy_to}/public_html"
 set :extensions, %w[plg_ie6 public template]
