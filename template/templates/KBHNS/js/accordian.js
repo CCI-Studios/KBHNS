b75f9d900a12df6f2b055e2ca84119c14bdb4fc1
Matt Elliott <melliott@ccistudios.com>
2012-03-02 15:26:07 -0500

    changed jquery accordion to mootools

--- template/templates/KBHNS/js/accordian.js
+++ template/templates/KBHNS/js/accordian.js
@@ -1,14 +1,14 @@
-(function() {
-	
-	$('dl.accordian').each(function() {
-		$(this).on('click', 'dt', function() {
-			$(this)
-				.next()
-					.slideDown(300)
-					.siblings('dd')
-					.slideUp(300);
-		});
-		$(this).find('dd').addClass('hidden');
+
+window.addEvent('domready', function() {
+	$$('dl.accordian').each(function(dl) {
+		var dts = dl.getElements('dt'),
+			dds = dl.getElements('dd'),
+			acc;
+			
+		acc = new Fx.Accordion(dts, dds, {
+			display: -1,
+			alwaysHide: true,
+			duration: 300
+		});		
 	});
-	
-})();
+});
