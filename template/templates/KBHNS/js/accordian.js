e97e3669ac8bed5fd6e93b9d91ae22c92df7f3bd
Matt Elliott <melliott@ccistudios.com>
2012-03-06 17:05:39 -0500

    injected arrow class in accordion dt's for arrow placement

--- template/templates/KBHNS/js/accordian.js
+++ template/templates/KBHNS/js/accordian.js
@@ -3,12 +3,17 @@ window.addEvent('domready', function() {
 	$$('dl.accordian').each(function(dl) {
 		var dts = dl.getElements('dt'),
 			dds = dl.getElements('dd'),
-			acc;
+			acc,
+			span = new Element('span', { 'class': 'arrow' });
+			
+		dts.each(function(dt) {
+			span.clone().inject(dt);
+		});
 			
 		acc = new Fx.Accordion(dts, dds, {
 			display: -1,
 			alwaysHide: true,
 			duration: 300
-		});		
+		});	
 	});
 });
